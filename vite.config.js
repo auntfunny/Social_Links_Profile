b8e60dd4c8fd235524994cd367a1ec796889c07b
auntfunny <greenauntfunny@gmail.com>
2026-02-03 17:37:50 -0300

    Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/Social_Links_Profile/', 
   plugins: [
     tailwindcss(),
   ],
